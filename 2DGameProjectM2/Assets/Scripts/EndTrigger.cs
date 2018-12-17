e9dab47928edee2f8e865b5f13690b6d57c9df7d
David Nolan <davidnolan458@gmail.com>
2018-12-17 16:49:38 +0000

    Complete
    Completed project added main music menu, way to mute music, way to win and a level complete screen.

---
+++ 2DGameProjectM2/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour {
+
+    public GameManager gameManager;
+
+
+ void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameManager.CompleteLevel();
+    }
+}
+
